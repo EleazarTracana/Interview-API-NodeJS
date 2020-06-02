92439389f38f1f8a1b99d3ed7bab948df46ef8a0
eleazartracana <eleazartracana@gmail.com>
2020-06-02 14:14:22 -0300

    se acomodan errores

--- Modulos/constantes.js
+++ Modulos/constantes.js
@@ -1,17 +1,17 @@
 
-const factory    = require('./models.js');
+const message    = require('./models.js').message;
 
 module.exports = {
 
-    invalid:          new factory.Message(true, "403", 'Invalid token'),
-    genericError:     new factory.Message(true, "500", 'An error has ocurred'),
-    candidateNoFound: new factory.Message(true, "404", 'Candidate not found'),
-    candidateExist:   new factory.Message(true, "403", 'Candidate cannot by added. Already Exist'),
-    paramNotFound:    new factory.Message(true, "404", 'Param not found'),
-    candidateAdded:   new factory.Message(false,"200", 'Your candidate has been added successfully'),
-    userNotFound:     new factory.Message(true, "404", 'User not found'),
-    incorrect:        new factory.Message(true, "403", 'User/password incorrect'),
+    invalid:          new message(true, "403", 'Invalid token'),
+    genericError:     new message(true, "500", 'An error has ocurred'),
+    candidateNoFound: new message(true, "404", 'Candidate not found'),
+    candidateExist:   new message(true, "403", 'Candidate cannot by added. Already Exist'),
+    paramNotFound:    new message(true, "404", 'Param not found'),
+    candidateAdded:   new message(false,"200", 'Your candidate has been added successfully'),
+    userNotFound:     new message(true, "404", 'User not found'),
+    incorrect:        new message(true, "403", 'User/password incorrect'),
     validated(token){
-        return new factory.Message(false,"200",token)
+        return new message(false,"200",token)
     }
 }
--- Modulos/models.js
+++ Modulos/models.js
@@ -1,19 +1,17 @@
 module.exports = {
-    Message : ResponseMessage,
-    Results : Results
-}
-class ResponseMessage{
-  constructor(error,codigo,mensaje) {
-      this.error  = error;
-      this.codigo = codigo;
-      this.mensaje = mensaje; 
+  message : class ResponseMessage{
+    constructor(error,codigo,mensaje) {
+        this.error  = error;
+        this.codigo = codigo;
+        this.mensaje = mensaje; 
+      }
+    },
+  results : class Results{
+    constructor(dni,technology,results,pk){
+      this.technology = technology,
+      this.candidate_id = dni,
+      this.results = results
+      this._id = pk
     }
-  }
-class Results{
-  constructor(dni,technology,results,pk){
-    this.technology = technology,
-    this.candidate_id = dni,
-    this.results = results
-    this._id = pk
-  }
+  }    
 }
