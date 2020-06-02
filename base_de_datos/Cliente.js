2f6b556e0390ee18bb267f286dce83360d610f07
eleazartracana <eleazartracana@gmail.com>
2020-06-02 14:00:34 -0300

    se actualiza conexion vieja

--- base_de_datos/Cliente.js
+++ base_de_datos/Cliente.js
@@ -1,5 +1,7 @@
 const MongoClient  = require('mongodb').MongoClient;
-const url          = "mongodb+srv://EleazarTracana:5q7f7EFKpyz1GuqC@cluster0-mvhqd.mongodb.net/test?retryWrites=true&w=majority";
+// conexion vieja const url          = "mongodb+srv://EleazarTracana:5q7f7EFKpyz1GuqC@cluster0-mvhqd.mongodb.net/test?retryWrites=true&w=majority";
+const url = "mongodb+srv://Colloquium:I37j2cOeQuzuMyyQ@cluster0-zu10c.mongodb.net/test?retryWrites=true&w=majority"
+
 module.exports = {
     conexion: async function Conectar() {   
         client = await MongoClient.connect(url, { useUnifiedTopology: true });
